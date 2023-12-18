7f16a2e22b7af6dbcf7771be076986ea1b8f9464
dimi3 <oresansky@gmail.com>
2023-12-18 23:07:08 +0100

    texty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,27 +3,46 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center" class="content">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-      <span style="display: block">{{ title }} app is running!</span>
-      <img width="300" alt="Angular Logo" src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9IjAgMCAyNTAgMjUwIj4KICAgIDxwYXRoIGZpbGw9IiNERDAwMzEiIGQ9Ik0xMjUgMzBMMzEuOSA2My4ybDE0LjIgMTIzLjFMMTI1IDIzMGw3OC45LTQzLjcgMTQuMi0xMjMuMXoiIC8+CiAgICA8cGF0aCBmaWxsPSIjQzMwMDJGIiBkPSJNMTI1IDMwdjIyLjItLjFWMjMwbDc4LjktNDMuNyAxNC4yLTEyMy4xTDEyNSAzMHoiIC8+CiAgICA8cGF0aCAgZmlsbD0iI0ZGRkZGRiIgZD0iTTEyNSA1Mi4xTDY2LjggMTgyLjZoMjEuN2wxMS43LTI5LjJoNDkuNGwxMS43IDI5LjJIMTgzTDEyNSA1Mi4xem0xNyA4My4zaC0zNGwxNy00MC45IDE3IDQwLjl6IiAvPgogIDwvc3ZnPg==">
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <ul>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/tutorial">Tour of Heroes</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://angular.io/cli">CLI Documentation</a></h2>
-      </li>
-      <li>
-        <h2><a target="_blank" rel="noopener" href="https://blog.angular.io/">Angular blog</a></h2>
-      </li>
-    </ul>
-    
+      <p>PRÁCE
+          O MNE
+          ODKAZY
+          KONTAKT:
+
+          DIMI3(ZAVINÁČ)
+          GRAFIKDIMI3.SK</p>
+      <p>grafikdimi3.sk</p>
+      <p>Grafika, webové a mobilné aplikácie, webové stránky</p>
+      <h2>Vybraté práce</h2>
+      <h3>Futbal HUB</h3>
+      <p>Webová aplikácia na zapisovanie futbalových výsledkov Futbal HUB. Postavená na Angular s PHP a mysql databázou na backende.</p>
+
+      <h3>Tartaria</h3>
+      <p>2D plošinovka v JavaScripte. Tartaria. Vytvorená za pomoci technológie Phaser.js v2.</p>
+
+      <h3>NG Mailer</h3>
+      <p>Webový editor pre tvorbu responzívnych html e-mailov. Pre spoločnosť ELET. Postavená na Angular a integrovaná do firemného ekosystému.</p>
+
+      <h3>Karty osudu</h3>
+      <p>Webová hra s tarotovými kartami Karty osudu. Jednoduché HTML, CSS (hlavne animácie) a trocha Javascriptu. Plus ľúbivý grafický design.</p>
+
+      <h3>PAGAN</h3>
+      <p>Webová fantasy hra. V spolupráci s Ján Repáň. Postavená na technológii angular, PHP a sqlite na backende. Hra nieje dokončená s dôvodu privysokých nárokov na čas. (Keďže sa jedná o hobby projekt). Možno v budúcnosti sa k tomu vrátim, ak niekto nájde čarovný stroj, ktorý preklopí AngularJS appku do Angular.</p>
+
+      <h3>Edenred Benefity</h3>
+      <p>Mobilná aplikácia pre Edenred Edenred Benefity.</p>
+
+      <h2>O mne</h2>
+      <p>Som grafik digitálnych médii a webový vývojár, a tvorbe sa venujem už 17 rokov. Najnovšie som k tomu pridal aj tvorbu webových a mobilných aplikácii. Verím že výsledný produkt je úspešný ak je pri jeho tvorbe súhra všetkých jeho častí z ktorých je zložený.</p>
+
+      <p>Github, LinkedIn, Twitter.</p>
+
+      <h2>Odkazy</h2>
+          <p>Responzívna e-mailová html predloha
+          http://tedgoas.github.io/Cerberus/
+          Svetlejšie/tmavšie farby v css premenných
+          https://kevingimbel.de/blog/2021/06/darken-and-lighten-colors-with-css/
+          Využitie interpolácie na webe (napr. pri animáciach)
+          https://www.trysmudford.com/blog/linear-interpolation-functions/</p>
   `,
   styles: []
 })
